a2f0b3a067dd9f7d229bb836ecd24e9459ee2c50
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-05-08 15:19:08 +0700

    membuat class orang

--- FriendClass/FriendClass.cpp
+++ FriendClass/FriendClass.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+using namespace std;
+
+class siswa;
+
+class orang {
+private:
+    string nama;
+public:
+    void setNama(string pnama);
+    friend class siswa;
+};
 
 int main()
 {
