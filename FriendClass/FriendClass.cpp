6c0dd023b0ff38fd4bfee00813ca35bc1a2230f4
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-05-08 15:30:57 +0700

    membuat method

--- FriendClass/FriendClass.cpp
+++ FriendClass/FriendClass.cpp
@@ -7,11 +7,31 @@ class orang {
 private:
     string nama;
 public:
-    void setNama(string pnama);
+    void setNama(string pNama);
     friend class siswa;
 };
 
+class siswa {
+private:
+    int id;
+public:
+    void setId(int pId);
+    void displayAll(orang& a);
+};
+
+void siswa::displayAll(orang& a) {
+    cout << id << endl << a.nama;
+}
+
+void siswa::setId(int pid) {
+    id = pid;
+}
+
+void orang::setNama(string pNama) {
+    nama = pNama;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
