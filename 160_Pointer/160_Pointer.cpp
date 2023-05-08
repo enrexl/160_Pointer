0a243d6b84ad603687740e0c666f96bcc6e3922e
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-05-08 14:54:36 +0700

    Membuat method diluar kelas

--- 160_Pointer/160_Pointer.cpp
+++ 160_Pointer/160_Pointer.cpp
@@ -7,6 +7,11 @@ public:
     void showNim(); //Deklarasi method
 }
 
+void mahasiswa::showNim() { //Implementasi methode diluar class
+    cout << "NO Induk = " << nim << endll;
+        }
+
+
 int main()
 {
     std::cout << "Hello World!\n";
