9dfd8358f3fe81d4943363d00c6d8a52312a2e35
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-05-08 14:52:32 +0700

    membuat kelas mahasiswa beserta membernya

--- 160_Pointer/160_Pointer.cpp
+++ 160_Pointer/160_Pointer.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 using namespace std;
+
+class mahasiswa {
+public:
+    int nim;
+    void showNim(); //Deklarasi method
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
