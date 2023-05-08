92fc82df7fbcf445fabe751422bfb5c84ab88a30
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-05-08 15:55:08 +0700

    Membuat Class Pelajar

--- FriendClass3/FriendClass3.cpp
+++ FriendClass3/FriendClass3.cpp
@@ -8,6 +8,16 @@ public:
     void showNilaiPelajar(pelajar&);
 };
 
+class pelajar {
+private:
+    int nilai;
+public:
+    pelajar() {
+        nilai = 100;
+    }
+    friend void manusia::showNilaiPelajar(pelajar& x);
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
